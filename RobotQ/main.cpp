4c8d5e5475fa4630d1314a851253277365a6a9d2
Mightypants <lbwanghr@163.com>
2018-07-25 13:37:16 +0800

    GUI_Test
    Loading pixmap accomplished.
    Loading webpage accomplished.
    Add some pixmaps.

--- RobotQ/main.cpp
+++ RobotQ/main.cpp
@@ -1,8 +1,10 @@
 #include "robotq.h"
 #include <QtGui/QApplication>
+#include <QtWebKit/QtWebKit>
+#include <QtWebKit/QWebView>
+#include <QUrl>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	QApplication a(argc, argv);
 	RobotQ w;
 	w.show();
