2e639178879df82ca9b362fe7bb2388b3fe1f823
Mightypants <lbwanghr@163.com>
2018-07-28 16:35:45 +0800

    Signal & Slots
    Review the usage of SIGNAL and SLOT by syntax "connect".

--- RobotQ/robotq.cpp
+++ RobotQ/robotq.cpp
@@ -1,12 +1,21 @@
 #include "robotq.h"
 
-RobotQ::RobotQ(QWidget *parent, Qt::WFlags flags)
-	: QMainWindow(parent, flags)
-{
+RobotQ::RobotQ(QWidget *parent, Qt::WFlags flags):QMainWindow(parent, flags){
 	ui.setupUi(this);
+	connect(ui.btnStart,SIGNAL(clicked(bool)),this,SLOT(OnStartClicked(bool)));
+	connect(ui.btnEnd,SIGNAL(clicked(bool)),this,SLOT(OnEndClicked(bool)));
+}
+RobotQ::~RobotQ(){
+
 }
 
-RobotQ::~RobotQ()
-{
+int RobotQ::OnStartClicked(bool checked){
+	ui.textStatus->setPlainText("Start");
 
+	return 0;
+}
+int RobotQ::OnEndClicked(bool checked){
+	ui.textStatus->setPlainText("End");
+
+	return 0;
 }
--- RobotQ/robotq.h
+++ RobotQ/robotq.h
@@ -4,13 +4,16 @@
 #include <QtGui/QMainWindow>
 #include "ui_robotq.h"
 
-class RobotQ : public QMainWindow
-{
+class RobotQ : public QMainWindow{
 	Q_OBJECT
 
 public:
 	RobotQ(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~RobotQ();
+
+private slots:
+	int OnStartClicked(bool checked);
+	int OnEndClicked(bool checked);
 
 private:
 	Ui::RobotQClass ui;
